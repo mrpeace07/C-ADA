703f16533976bf659e6d4fa4fe1253e570757764
mrpeace07 <140835316+mrpeace07@users.noreply.github.com>
2023-07-30 18:00:45 +0530

    Update travel75.c

--- travel75.c
+++ travel75.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
-int s, c[100][100], ver;
+int c[100][100], s, ver, path[10];
 float optimum = 999, sum;
 
-void swap(int v[], int i, int j)
+int swap(int v[], int i, int j)
 {
     int t;
     t = v[i];
@@ -13,7 +12,7 @@ void swap(int v[], int i, int j)
 
 void tsp(int v[], int n, int i)
 {
-    int j, sum1, k;
+    int sum1, j, k;
     if (i == n)
     {
         if (v[0] == s)
@@ -26,7 +25,11 @@ void tsp(int v[], int n, int i)
             }
             sum1 = sum1 + c[v[n - 1]][s];
             if (sum1 < optimum)
+            {
                 optimum = sum1;
+                for (k = 0; k < n; k++)
+                    path[k] = v[k];
+            }
         }
     }
     else
@@ -47,43 +50,51 @@ void approx(int ver)
         vis[i] = 0;
     vis[s] = 1;
     from = s;
+    printf("the approx.path is %d", s);
     sum = 0;
     for (j = 1; j < ver; j++)
     {
         min = 999;
         for (i = 1; i <= ver; i++)
-        {
             if (vis[i] != 1 && c[from][i] < min && c[from][i] != 0)
             {
                 min = c[from][i];
-                p = i;
+                p = i;  
             }
-        }
         vis[p] = 1;
         from = p;
+        printf("-->%d", p);
         sum = sum + min;
     }
     sum = sum + c[from][s];
+    printf("-->%d\n", s);
 }
 
 int main()
 {
-    int v[100], i, j;
-    printf("Enter n: ");
+    int v[100], ver, i, j;
+    printf("Enter No. of vertices: ");
     scanf("%d", &ver);
     for (i = 0; i < ver; i++)
         v[i] = i + 1;
-    printf("Enter cost matrix (999 for infinity):\n");
+    printf("Enter the Cost matrix \n");
     for (i = 1; i <= ver; i++)
         for (j = 1; j <= ver; j++)
+        {
             scanf("%d", &c[i][j]);
-    printf("\nEnter source: ");
+            if (c[i][j] == 0)
+                c[i][j] = 999;
+        }
+    printf("Enter Source: ");
     scanf("%d", &s);
     tsp(v, ver, 0);
-    printf("\nOptimum solution is = %.2f\n", optimum);
+    printf("Shortest path for Optimal Solution: ");
+    for (i = 0; i < ver; i++)
+        printf("%d -> ", path[i]);
+    printf("%d\n", path[0]);
+    printf("\nOptimum solution is = %f\n", optimum);
     approx(ver);
-    printf("\nSolution with Approximation Algorithm is = %.2f\n", sum);
-    printf("\nThe approximation value is = %.2f%%\n", ((sum / optimum) - 1) * 100);
-
+    printf("Solution with Approximation Algorithm is = %f\n", sum);
+    printf("The approximation value is = %.2f\n", ((sum / optimum) - 1) * 100);
     return 0;
 }
