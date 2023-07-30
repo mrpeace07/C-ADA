4a6c63f96c5941d86a279ae15847c99f8cd589ce
mrpeace07 <140835316+mrpeace07@users.noreply.github.com>
2023-07-30 07:40:24 +0530

    Update topology.c

--- topology.c
+++ topology.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <conio.h>
-int a[10][10], n, indegre[10];
-void find_indegre()
+
+int a[10][10], n, indegree[10];
+
+void find_indegree()
 {
     int j, i, sum;
     for (j = 0; j < n; j++)
@@ -9,16 +11,17 @@ void find_indegre()
         sum = 0;
         for (i = 0; i < n; i++)
             sum += a[i][j];
-        indegre[j] = sum;
+        indegree[j] = sum;
     }
 }
+
 void topology()
 {
     int i, u, v, t[10], s[10], top = -1, k = 0;
-    find_indegre();
+    find_indegree();
     for (i = 0; i < n; i++)
     {
-        if (indegre[i] == 0)
+        if (indegree[i] == 0)
             s[++top] = i;
     }
     while (top != -1)
@@ -29,20 +32,21 @@ void topology()
         {
             if (a[u][v] == 1)
             {
-                indegre[v]--;
-                if (indegre[v] == 0)
+                indegree[v]--;
+                if (indegree[v] == 0)
                     s[++top] = v;
             }
         }
     }
     printf("The topological Sequence is:\n");
     for (i = 0; i < n; i++)
-        printf("%d ", t[i]);
+        printf("%d ", t[i] + 1); // Add 1 to the index to get the original job number
 }
+
 int main()
 {
     int i, j;
-    printf("Enter number of jobs:");
+    printf("Enter number of nodes:");
     scanf("%d", &n);
     printf("\nEnter the adjacency matrix:\n");
     for (i = 0; i < n; i++)
